9bcc5bc2a9572b44dff7cc5e5ddcf228b2db39b2
DanaLevi005 <danapearl.levi@post.runi.ac.il>
2023-12-28 20:53:30 +0200

    test4

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,16 +5,18 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
         //iterate through the pattern of the damka rows 
-		for (int i = 0; i < n / 2; i++) {
+		for (int i = 0; i < n ; i++) {
 			//iterate through the pattern of the damka columns
 			for (int j = 0; j < n; j++) {
-				System.out.print("* ");
-			}
-			System.out.println();
-			for (int k = 0; k < n; k++){
-				System.out.print(" *");
-			}
-			System.out.println();
-		}
-	}
-}
+				if (i % 2 == 0){
+					System.out.print("* ");
+				} else {
+					System.out.print(" *");
+				}
+
+		    }
+		    System.out.println();
+
+	    }
+    }
+}    
