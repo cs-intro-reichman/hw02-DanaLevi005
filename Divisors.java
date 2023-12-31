6cfe162179be6e97ea5bd3e5ac275d606162517e
DanaLevi005 <danapearl.levi@post.runi.ac.il>
2023-12-31 21:02:44 +0200

    test5

--- Divisors.java
+++ Divisors.java
@@ -4,6 +4,9 @@
 public class Divisors {
 	public static void main (String[] args) {
 		int number = Integer.parseInt(args[0]);
+		if (number == 0) {
+			System.out.println(number);
+		}
 		for (int i = 1; i < number++; i++) {
 			if (number % i == 0) {
 				System.out.println(i);
